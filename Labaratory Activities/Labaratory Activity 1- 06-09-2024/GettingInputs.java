8f72e4dff0f26f1b997bf154728a3922b21eaea3
Justine-Lance-Pabalan <165749021+Justine-Lance-Pabalan@users.noreply.github.com>
2024-09-06 07:12:04 +0000

    TAPOS NA

--- Labaratory Activities/Labaratory Activity 1- 06-09-2024/GettingInputs.java
+++ Labaratory Activities/Labaratory Activity 1- 06-09-2024/GettingInputs.java
@@ -4,27 +4,27 @@ public class GettingInputs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the year: ");
+        System.out.print("Enter the year: ");
         String year = scanner.nextLine();
 
-        System.out.println("Enter the genre: ");
+        System.out.print("Enter the genre: ");
         String genre = scanner.nextLine();
 
-        System.out.println("Enter the album: ");
+        System.out.print("Enter the album: ");
         String album = scanner.nextLine();
 
-        System.out.println("Enter the title: ");
+        System.out.print("Enter the title: ");
         String title = scanner.nextLine();
 
-        System.out.println("Enter the artist: ");
+        System.out.print("Enter the artist: ");
         String artist = scanner.nextLine();
 
-        System.out.println("---------------\nSONG DETAILS\n---------------")
+        System.out.println("-------------------------\nSONG DETAILS\n-------------------------");
 
         System.out.println("Year Released: "+ year);
         System.out.println("Genre: "+ genre);
         System.out.println("Album: "+ album); 
-        System.out.println("Title: "+ title);
+        System.out.println("Title: \""+ title +"\"");
         System.out.println("Artist: "+ artist);
         
         scanner.close();
