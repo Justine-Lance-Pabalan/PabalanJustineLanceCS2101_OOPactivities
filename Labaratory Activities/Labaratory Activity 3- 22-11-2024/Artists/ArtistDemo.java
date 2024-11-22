566f57f03eb127420dbcd57f6f625af182e80202
Justine-Lance-Pabalan <165749021+Justine-Lance-Pabalan@users.noreply.github.com>
2024-11-22 08:43:14 +0000

    final

--- Labaratory Activities/Labaratory Activity 3- 22-11-2024/Artists/ArtistDemo.java
+++ Labaratory Activities/Labaratory Activity 3- 22-11-2024/Artists/ArtistDemo.java
@@ -8,13 +8,21 @@ public class ArtistDemo {
         Singer singer = new Singer("Nayeon Im", "Korean", 29, "Music", Genre.POP);
         singer.displayInfo();
 
+        System.out.println();
+
         Dancer dancer = new Dancer("Misty Copeland", "American", 42, "Dance", DanceStyle.BALLET);
         dancer.displayInfo();
+
+        System.out.println(null);
 
         Painter painter = new Painter("Gerhard Richter", "German", 92, "Painting", Medium.OIL);
         painter.displayInfo();
 
+        System.out.println();
+
         Writer writer = new Writer("Miguel Syjuco", "Filipino", 46, "Writing", WritingStyle.FICTION);
         writer.displayInfo();
+
+        System.out.println(null);
     }
 }
